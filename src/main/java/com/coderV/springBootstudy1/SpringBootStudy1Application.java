4c497cc08035fb694a647d2b056f84fbf52c23cc
gxx <626984947@qq.com>
2018-08-31 16:32:11 +0800

    第一次从start.spring.io上初始化项目

---
+++ src/main/java/com/coderV/springBootstudy1/SpringBootStudy1Application.java
@@ -0,0 +1,12 @@
+package com.coderV.springBootstudy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudy1Application.class, args);
+	}
+}
